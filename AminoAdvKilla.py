d8de5ed16c42d30e1900eeaaab9e027c7f233f7c
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-04-27 20:31:09 +0600

    Update AminoAdvKilla.py

--- AminoAdvKilla.py
+++ AminoAdvKilla.py
@@ -1,4 +1,4 @@
-import aminofix
+import amino
 from concurrent.futures import ThreadPoolExecutor
 print("""Script by deluvsushi
 Github : https://github.com/deluvsushi
@@ -9,7 +9,7 @@ Github : https://github.com/deluvsushi
 ┃╭━╮┃┃┃┃┃┃┃┃╰╯┃╭━╮┃╰╯┣╮╭┫┃┃╰┫┃╰┫╰┫╭╮┃
 ╰╯╱╰┻┻┻┻┻╯╰┻━━┻╯╱╰┻━━╯╰╯╰╯╰━┻┻━┻━┻╯╰╯
 """)
-client = aminofix.Client()
+client = amino.Client()
 email = input("-- Email::: ")
 password = input("-- Password::: ")
 client.login(email=email, password=password)
@@ -17,18 +17,15 @@ clients = client.sub_clients(start=0, size=100)
 for x, name in enumerate(clients.name, 1):
 	print(f"-- {x}:{name}")
 com_id = clients.comId[int(input("-- Select the community::: ")) - 1]
-sub_client = aminofix.SubClient(comId=com_id, profile=client.profile)
-
-def global_advertise(message: str):
-	while True:
+sub_client = amino.SubClient(comId=com_id, profile=client.profile)
+message = input("-- Message::: ")
+while True:
+	with ThreadPoolExecutor(max_workers=100) as executor:
 		for i in range(0, 250, 20000):
 			try:
 				online_users = sub_client.get_online_users(start=i, size=100).profile.userId
 				print(f"-- Advertising...")
 				client.start_chat(userId=online_users, message=message)
-				with ThreadPoolExecutor(max_workers=100) as executor:
-					[executor.submit(client.start_chat, online_users, message) for user_id in online_users]
+				[executor.submit(client.start_chat, online_users, message) for user_id in online_users]
 			except Exception as e:
 				print(e)
-
-global_advertise(message=input("-- Message::: "))
